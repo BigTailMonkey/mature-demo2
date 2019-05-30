7a544868d66750f548692aea3426c9aa6e4d2ca0
BigTailMonkey <hdw_worker@163.com>
2019-05-30 14:31:01 +0800

    完善数据库访问配置文件

--- src/main/java/com/btm/maturedemo2/codeGenerate/configuration/Configure.java
+++ src/main/java/com/btm/maturedemo2/codeGenerate/configuration/Configure.java
@@ -7,10 +7,10 @@ package com.btm.maturedemo2.codeGenerate.configuration;
  */
 public class Configure {
 
-    private final static String DRIVER  = "com.mysql.cj.jdbc.Driver";
-    private final static String URL  = "jdbc:mysql://localhost:3306/btm?characterEncoding=UTF8&useSSL=false";
-    private final static String USER_NAME  = "";
-    private final static String PASSWORD = "";
+    public final static String DRIVER  = "com.mysql.cj.jdbc.Driver";
+    public final static String URL  = "jdbc:mysql://localhost:3306/btm?characterEncoding=UTF8&useSSL=false";
+    public final static String USER_NAME  = "";
+    public final static String PASSWORD = "";
 
 
 
