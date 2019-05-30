f2f273bc3b02bd42229c4e7a55af7b76b0dcff93
BigTailMonkey <hdw_worker@163.com>
2019-05-30 14:00:14 +0800

    添加数据库配置信息维护类

---
+++ src/main/java/com/btm/maturedemo2/codeGenerate/configuration/Configure.java
@@ -0,0 +1,17 @@
+package com.btm.maturedemo2.codeGenerate.configuration;
+
+/**
+ * @Author: BigTailMonkey
+ * @Date: 2019/5/30 13:57
+ * @Version: 1.0
+ */
+public class Configure {
+
+    private final static String DRIVER  = "";
+    private final static String URL  = "";
+    private final static String USER_NAME  = "";
+    private final static String PASSWORD = "";
+
+
+
+}
