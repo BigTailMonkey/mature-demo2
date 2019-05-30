3e0b0073b7fccc0464d76a4416bea37e2fd702ca
BigTailMonkey <hdw_worker@163.com>
2019-05-30 08:59:01 +0800

    完善date_query分支备注

--- src/main/java/com/btm/maturedemo2/controller/dataQuery/DateController.java
+++ src/main/java/com/btm/maturedemo2/controller/dataQuery/DateController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
+ * 时间 单表 单条件查询
  * @Author: BigTailMonkey
  * @Date: 2019/5/29 21:09
  * @Version: 1.0
@@ -22,6 +23,16 @@ public class DateController {
     @Autowired
     private UserMapper userMapper;
 
+    /**
+     * 查询结果中的时间字段精度由实体中相应字段的类型决定
+     * 具体请参考 com.btm.maturedemo2.model.ds1.User
+     *
+     * 单条件 时间条件
+     * 条件形式：param1=value1
+     * 多结果 包含时间字段
+     *
+     * @return
+     */
     @GetMapping("dateResultDateConditional")
     public String dateResultDateConditional(){
         //声明一个查询条件包装器
